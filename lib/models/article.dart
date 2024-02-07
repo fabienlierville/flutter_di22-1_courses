bfdea19a785071a62e9af67d5fe6d2a23876c2a3
fabien <flierville@cesi.fr>
2024-02-07 09:03:32 +0100

    Modèles

---
+++ lib/models/article.dart
@@ -0,0 +1,35 @@
+class Article{
+  int? id;
+  String nom;
+  double prix;
+  String image;
+  int magasin; //Sera notre clef étrangère
+
+  Article({
+    this.id,
+    required this.nom,
+    required this.prix,
+    required this.image,
+    required this.magasin,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'nom': this.nom,
+      'prix': this.prix,
+      'image': this.image,
+      'magasin': this.magasin,
+    };
+  }
+
+  factory Article.fromMap(Map<String, dynamic> map) {
+    return Article(
+      id: map['id'] as int,
+      nom: map['nom'] as String,
+      prix: map['prix'] as double,
+      image: map['image'] as String,
+      magasin: map['magasin'] as int,
+    );
+  }
+}
