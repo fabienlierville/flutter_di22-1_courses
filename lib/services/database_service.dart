27c3e4f42ec3c831b0ba79debfba94a29d7b6834
fabien <flierville@cesi.fr>
2024-02-07 09:20:52 +0100

    create database

---
+++ lib/services/database_service.dart
@@ -0,0 +1,29 @@
+
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseService{
+  static Database? _database;
+
+  // Getter qui donne la connexion (et la créé si null)
+  static Future<Database> get database async{
+    if(_database == null){
+      _database = await _opendb();
+    }
+    return _database!;
+  }
+
+  static Future<Database> _opendb() async{
+    // Si problème regarder ici absolument
+    Database bdd = await openDatabase("/data/database.db", version: 1, onCreate: _onCreate);
+    return bdd;
+  }
+
+  static Future<void> _onCreate(Database bdd, int version) async{
+    await bdd.execute('''
+      CREATE TABLE magasin(
+        id INTEGER PRIMARY KEY,
+        nom VARCHAR(50) NOT NULL
+      )
+    ''');
+  }
+}
